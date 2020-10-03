6d927aecfad705a1a944740a796aa13c3f388cee
joshuaonazi <52982372+joshuaonazi@users.noreply.github.com>
2020-10-03 22:10:41 +0100

    Added luhncheck

---
+++ Luhncheck.js
@@ -0,0 +1,13 @@
+const luhnCheck = num => {
+  let arr = (num + '')
+    .split('')
+    .reverse()
+    .map(x => parseInt(x));
+  let lastDigit = arr.splice(0, 1)[0];
+  let sum = arr.reduce((acc, val, i) => (i % 2 !== 0 ? acc + val : acc + ((val * 2) % 9) || 9), 0);
+  sum += lastDigit;
+  return sum % 10 === 0;
+};
+console.log(luhnCheck('4485275742308327'));
+console.log(luhnCheck(6011329933655299));
+console.log(luhnCheck(123456789));
